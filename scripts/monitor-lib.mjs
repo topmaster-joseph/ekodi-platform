78c21ca83faeb879e446d29641262cd351f4ce74
topmaster-joseph <topmaster.joseph@gmail.com>
2026-08-12 22:30:58 +0900

    마케팅AI와 청계상권을 통합 모니터링에 추가

--- scripts/monitor-lib.mjs
+++ scripts/monitor-lib.mjs
@@ -1,6 +1,8 @@
 export const SITE_DEFINITIONS = Object.freeze([
   ['mall', '에코디몰', 'mall.ekodi.kr'],
   ['biz', '에코디비즈', 'biz.ekodi.kr'],
+  ['marketing', '마케팅AI', 'marketing.ekodi.kr'],
+  ['cgma', '청계상권', 'cgma.ekodi.kr'],
   ['publishing', '에코디출판', 'books.ekodi.kr'],
   ['church', '에코디교회', 'church.ekodi.kr'],
   ['lab', '에코디연구소', 'lab.ekodi.kr'],
