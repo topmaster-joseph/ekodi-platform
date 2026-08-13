9b4bcafd573748e2d72c6facc14d82fe44b455af
topmaster-joseph <topmaster.joseph@gmail.com>
2026-08-13 11:52:59 +0900

    feat: add isolated client auth realm to central router

--- auth-site/auth-router.js
+++ auth-site/auth-router.js
@@ -1,3 +1,4 @@
 const site=new URLSearchParams(location.search).get('site');
 if(site==='admin') await import('./admin-auth.js');
+else if(site==='cgma-client') await import('./client-auth.js');
 else await import('./auth.js');
