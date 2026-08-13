15f431c2c83ff2e0f79461ca5c248dbf6eb0c7cb
topmaster-joseph <topmaster.joseph@gmail.com>
2026-08-13 14:08:07 +0900

    Route managed customer realms through central Google identity

--- auth-site/auth-router.js
+++ auth-site/auth-router.js
@@ -1,4 +1,4 @@
 const site=new URLSearchParams(location.search).get('site');
 if(site==='admin') await import('./admin-auth.js');
-else if(site==='cgma-client') await import('./client-auth.js');
+else if(site==='cgma-client'||site==='jadam-client'||site==='pizzamaru-client'||site==='yogurt-client') await import('./client-auth.js');
 else await import('./auth.js');
