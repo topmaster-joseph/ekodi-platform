c368ba3a82624eaec9a6d6a4cf3a9766f5f426f2
topmaster-joseph <topmaster.joseph@gmail.com>
2026-08-15 02:17:56 +0900

    refactor: serve Books Social renderer as a static page asset

--- books-worker.js
+++ books-worker.js
@@ -1,10 +1,3 @@
-function enhanceHtml(response) {
-  if (!response.headers.get('content-type')?.includes('text/html')) return response;
-  return new HTMLRewriter()
-    .on('body', { element(el) { el.append('<script src="/social-links.js" defer></script>', { html: true }); } })
-    .transform(response);
-}
-
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
@@ -16,6 +9,6 @@ export default {
     if (url.pathname === '/admin' || url.pathname === '/admin/' || url.pathname === '/admin.html') {
       return Response.redirect('https://admin.ekodi.kr/books#books', 307);
     }
-    return enhanceHtml(await env.ASSETS.fetch(request));
+    return env.ASSETS.fetch(request);
   },
 };
