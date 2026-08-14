ea7c52d634458daaa73d2d91cfd2f6c33d0e0c70
topmaster-joseph <topmaster.joseph@gmail.com>
2026-08-14 10:08:21 +0900

    Load free-first Marketing AI onboarding

--- auth-site/auth-router.js
+++ auth-site/auth-router.js
@@ -1,4 +1,7 @@
 const site=new URLSearchParams(location.search).get('site');
 if(site==='admin') await import('./admin-auth.js');
 else if(site==='cgma-client'||site==='jadam-client'||site==='pizzamaru-client'||site==='yogurt-client') await import('./client-auth.js');
-else await import('./auth.js');
+else {
+  await import('./auth.js');
+  if(site==='marketing') await import('./marketing-onboarding.js');
+}
