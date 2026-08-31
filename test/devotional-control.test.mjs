36789d068ab8b3811acae5b48480311be7afa70a
topmaster-joseph <topmaster.joseph@gmail.com>
2026-09-01 01:26:15 +0900

    test: lock devotional passage order

--- test/devotional-control.test.mjs
+++ test/devotional-control.test.mjs
@@ -5,7 +5,10 @@ import { DEVOTIONAL_SEPTEMBER_2026, handleDevotionalControl } from '../devotiona
 test('September 2026 devotional plan contains 30 ordered passages', () => {
   assert.equal(DEVOTIONAL_SEPTEMBER_2026.length, 30);
   assert.equal(DEVOTIONAL_SEPTEMBER_2026[0], '신명기 14:22-29');
-  assert.equal(DEVOTIONAL_SEPTEMBER_2026[29], '신명기 28:15-26');
+  assert.equal(DEVOTIONAL_SEPTEMBER_2026[18], '신명기 23:1-8');
+  assert.equal(DEVOTIONAL_SEPTEMBER_2026[19], '신명기 23:9-14');
+  assert.equal(DEVOTIONAL_SEPTEMBER_2026[21], '신명기 24:1-13');
+  assert.equal(DEVOTIONAL_SEPTEMBER_2026[29], '신명기 28:1-14');
 });
 
 test('devotional control ignores unrelated routes before authentication', async () => {
