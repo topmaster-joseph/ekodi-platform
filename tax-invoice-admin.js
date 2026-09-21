4c98f98c500fe8e4f8d22f8c7e172fdf87134fde
topmaster-joseph <213826185+topmaster-joseph@users.noreply.github.com>
2026-09-22 01:24:25 +0900

    refactor(api): use apex Finance API

--- tax-invoice-admin.js
+++ tax-invoice-admin.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const FINANCE_API = 'https://finance-api.ekodi.kr';
+  const FINANCE_API = 'https://ekodi.kr';
   const ORGANIZATION_ID = 'EKODIBIZ';
   const DEFAULT_UNIT = 'BIZ';
   const HOMETAX_URL = 'https://www.hometax.go.kr';
